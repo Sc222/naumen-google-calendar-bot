176d0dda38768d6b0023fb76c9f71a9a1cf46bed
Sc222 <v.a.shaternikov@gmail.com>
2021-03-04 19:35:19 +0500

    spring initializr template

---
+++ src/main/java/ru/dudes/google_calendar_helper/GoogleCalendarHelperApplication.java
@@ -0,0 +1,13 @@
+package ru.dudes.google_calendar_helper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoogleCalendarHelperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoogleCalendarHelperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/dudes/google_calendar_helper/GoogleCalendarHelperApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.dudes.google_calendar_helper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoogleCalendarHelperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
